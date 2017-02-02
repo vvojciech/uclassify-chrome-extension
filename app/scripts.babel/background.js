edbdfcf963815fd6eb58d62e5200029f9bc9899f
Wojciech Kucha <wojciech.kucha@simplybusiness.co.uk>
2017-02-02 14:24:17 +0000

    adding classificators + some DRYing out

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -15,86 +15,29 @@ function getUrl(url, callback) {
   request.send();
 }
 
-
-function gender(text, key) {
-  getUrl(`https://api.uclassify.com/v1/uclassify/genderanalyzer_v5/classify/?readKey=${key}&text=${encodeURI(text)}`,
-    result => {
-      chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-        chrome.tabs.sendMessage(tabs[0].id, {
-          action: 'add_gender', message: result
-        }, response => {
-        });
-      });
-    })
-}
-
-
-function sentiment(text, key) {
-  getUrl(`https://api.uclassify.com/v1/uclassify/sentiment/classify/?readKey=${key}&text=${encodeURI(text)}`,
-    result => {
-      chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-        chrome.tabs.sendMessage(tabs[0].id, {
-          action: 'add_sentiment', message: result
-        }, response => {
-        });
-      });
-    })
-}
-
-function age(text, key) {
-  getUrl(`https://api.uclassify.com/v1/uclassify/ageanalyzer/classify/?readKey=${key}&text=${encodeURI(text)}`,
-    result => {
-      chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-        chrome.tabs.sendMessage(tabs[0].id, {
-          action: 'add_age', message: result
-        }, response => {
-        });
-      });
-    })
-}
-
-function myers_briggs_attitude(text, key) {
-  getUrl(`https://api.uclassify.com/v1/prfekt/myers-briggs-attitude/classify/?readKey=${key}&text=${encodeURI(text)}`,
-    result => {
-      chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-        chrome.tabs.sendMessage(tabs[0].id, {
-          action: 'add_myers_briggs_attitude', message: result
-        }, response => {
-        });
-      });
-    })
-}
-
-function tonality(text, key) {
-  getUrl(`https://api.uclassify.com/v1/prfekt/tonality/classify/?readKey=${key}&text=${encodeURI(text)}`,
-    result => {
-      chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-        chrome.tabs.sendMessage(tabs[0].id, {
-          action: 'add_tonality', message: result
-        }, response => {
-        });
-      });
-    })
-}
-
 function getClassification(classifier, key, text) {
-
+  getUrl(`https://api.uclassify.com/v1/${classifier}/classify/?readKey=${key}&text=${encodeURI(text)}`,
+    result => {
+      chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          classifier: classifier,
+          message: result
+        }, response => {
+        });
+      });
+    }
+  );
 }
 
 function uclassifyHandler(info) {
   const text = info.selectionText;
-
   chrome.storage.sync.get({
-    key: ''
+    key: '',
+    classifiers: []
   }, items => {
-
-    // TODO: add some validation for key
-
-    gender(text, items.key);
-    sentiment(text, items.key)
-    age(text, items.key)
-    myers_briggs_attitude(text, items.key)
-    tonality(text, items.key)
+    items.classifiers.forEach((element) => {
+      getClassification(element, items.key, text)
+    });
   });
 }
 
--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -2,16 +2,16 @@
 
 chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
   console.log(request);
-  switch (request.action) {
-    case 'add_gender':
+  switch (request.classifier) {
+    case 'uclassify/genderanalyzer_v5':
       addText('gender.male', request.message.male);
       addText('gender.female', request.message.female);
       break;
-    case 'add_sentiment':
+    case 'uclassify/sentiment':
       addText('sentiment.positive', request.message.positive);
       addText('sentiment.negative', request.message.negative);
       break;
-    case 'add_age':
+    case 'uclassify/ageanalyzer':
       addText('age.13-17', request.message['13-17']);
       addText('age.18-25', request.message['18-25']);
       addText('age.26-35', request.message['26-35']);
@@ -19,11 +19,23 @@ chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
       addText('age.51-64', request.message['51-65']);
       addText('age.65-100', request.message['65-100']);
       break;
-    case 'add_myers_briggs_attitude':
+    case 'prfekt/myers-briggs-attitude':
       addText('myers_briggs_attitude.Extraversion', request.message.Extraversion);
       addText('myers_briggs_attitude.Introversion', request.message.Introversion);
       break;
-    case 'add_tonality':
+    case 'prfekt/myers-briggs-judging-function':
+      addText('myers_briggs_attitude.Feeling', request.message.Feeling);
+      addText('myers_briggs_attitude.Thinking', request.message.Thinking);
+      break;
+    case 'prfekt/myers-briggs-lifestyle':
+      addText('myers_briggs_attitude.Judging', request.message.Judging);
+      addText('myers_briggs_attitude.Perceiving', request.message.Perceiving);
+      break;
+    case 'prfekt/myers-briggs-perceiving-function':
+      addText('myers_briggs_attitude.Sensing', request.message.Sensing);
+      addText('myers_briggs_attitude.Intuition', request.message.iNtuition);
+      break;
+    case 'prfekt/tonality':
       addText('tonality.Corporate', request.message.Corporate);
       addText('tonality.Personal', request.message.Personal);
       break;
@@ -43,7 +55,7 @@ function addText(label, text) {
         right: 0,
         padding: '10px',
         zIndex: 100000,
-      })
+      });
     $div.appendTo('body');
   }
 
